11c98722a44cd5ccc3ad96c89320ee584660b06d
SeungDae321 <seungdae33@gmail.com>
2024-01-30 22:26:48 +0900

    made the checkbox function

--- index.js
+++ index.js
@@ -9,16 +9,28 @@ const text = document.querySelector('.text');
 //when clicked send button
 const send = document.querySelector('.send');
 send.addEventListener('click', ()=>{
-    newList(text.value, lists);
-    text.value='';
+    if(text.value){
+        newList(text.value, lists);
+        text.value='';
+    }
+    
 })
 
 //make new list function. -It goes to "send" btn event listener-
 const newList = (text, place)=>{
     const list = document.createElement('li')
     list.innerText = text;
+    addCB(list)
     place.appendChild(list)
     addDelBtn(list, text)
+    
+}
+
+//add checkbox fucntion on "mak new list function"
+const addCB = (elem)=>{
+    const checkbox = document.createElement('input');
+    checkbox.setAttribute('type','checkbox');
+    elem.appendChild(checkbox)
 }
 
 //add delete button on "make new list function".
@@ -33,5 +45,11 @@ lists.addEventListener('click',(evt)=>{
     if(evt.target.tagName === 'BUTTON'){
      const deletedLi = evt.target.closest('li');
     deletedLi.remove()
+    }
+
+    if(evt.target.tagName === 'INPUT'){
+        const checkedLI = evt.target.closest('li');
+        const delBtn = evt.target.closest('button');
+        checkedLI.classList.toggle('checked')
     }
  })
